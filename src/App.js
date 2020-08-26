901fee2da8405913b76088e2e24a1719bce0d1d1
Aleksey Afanaskin <aleksey.afanaskin@gmail.com>
2020-08-26 20:54:45 +0300

    layout home page

--- src/App.js
+++ src/App.js
@@ -10,12 +10,43 @@ const App = () => {
 
       <nav className="nav">
         <div>
-          Profile
+          <a href="#">Profile</a>
+        </div>
+        <div>
+          <a href="#">Messages</a>
+        </div>
+        <div>
+          <a href="#">News</a>
+        </div>
+        <div>
+          <a href="#">Music</a>
+        </div>
+        <div>
+          <a href="#">Settings</a>
         </div>
       </nav>
 
       <div className="content">
-        Main content
+        <div>
+          <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" alt="image"/>
+        </div>
+        <div>
+          ava + disc
+        </div>
+        <div>
+          my post
+          <div>
+            new post
+          </div>
+          <div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
