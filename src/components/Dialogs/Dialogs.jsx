c9036ce9dec44040ae060204933965dd2779296a
af4n <aleksey.afanaskin@gmail.com>
2020-09-22 18:20:44 +0300

    layout page dialogs was added

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,26 @@ import styles from "./Dialogs.module.css"
 
 const Dialogs = (props) => {
   return (
-    <div>
-      Dialogs
+    <div className={styles.dialogs}>
+      <div className={styles.dialogsItems}>
+        <div className={styles.dialog}>
+          Dima
+        </div>
+        <div className={styles.dialog + ' ' + styles.active}>
+          Leha
+        </div>
+        <div className={styles.dialog}>
+          Ilona
+        </div>
+        <div className={styles.dialog}>
+          Dimych
+        </div>
+      </div>
+      <div className={styles.messages}>
+        <div className={styles.message}>Hi</div>
+        <div className={styles.message}>How is yuor</div>
+        <div className={styles.message}>Yo</div>
+      </div>
     </div>
   )
 }
