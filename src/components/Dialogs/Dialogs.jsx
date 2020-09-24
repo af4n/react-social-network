ebd45677c7545b434daa6785a80ae1c59018b503
af4n <aleksey.afanaskin@gmail.com>
2020-09-24 12:47:47 +0300

    dialogs route was added

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import styles from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
   return (
     <div className={styles.dialogs}>
       <div className={styles.dialogsItems}>
         <div className={styles.dialog}>
-          Dima
+          <NavLink to="/dialogs/1">Dima</NavLink>
         </div>
         <div className={styles.dialog + ' ' + styles.active}>
-          Leha
+          <NavLink to="/dialogs/2">Leha</NavLink>
         </div>
         <div className={styles.dialog}>
-          Ilona
-        </div>
-        <div className={styles.dialog}>
-          Dimych
+          <NavLink to="/dialogs/3">Ilona</NavLink>
         </div>
       </div>
       <div className={styles.messages}>
