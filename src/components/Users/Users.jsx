419bcc98b24288c17cb89490707d1a6e960c8474
af4n <aleksey.afanaskin@gmail.com>
2020-10-08 10:58:38 +0300

    users api was added

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import styles from "./Users.module.css"
+import * as axios from "axios";
+import userPhoto from "../../assets/images/user-photo.png"
 
 let Users = (props) => {
 
   if (props.users.length === 0) {
-    props.setUsers([
-      { id: 1, followed: false,  fullName: 'Alexey', status: 'I am a boss!', location: {city: 'Kiev', country: 'Ukraine'},
-        photoUrl: 'https://lh3.googleusercontent.com/proxy/8tVV8AE9ZydHcHauEm3mh-VhTFai5gEdy_YOz9EHx2b4Rjdclu34u_xhC5x4B69L50-J5rah_4IBAk2KxIwaYwVEWE_K-6aYRjhvsHOfZu9GuFArx4k9_R2vbwi2Gs5GiYUtXJ40AjPI79-CaSZXw3Dz-0A' },
-      { id: 2, followed: true, fullName: 'Ilona', status: 'I am a boss too!', location: {city: 'Poltava', country: 'Ukraine'},
-        photoUrl: 'https://lh3.googleusercontent.com/proxy/QAqVGbdZuQ-lwh88NVJIrkcnEv2aVQD_UseQoXzoAmktD1Yv5PTRDgw4ESDcp1hE-uSqo4U70thKqLmONdU5Bwkp7s79093LL8yI83q0M3xy' },
-      { id: 3, followed: false, fullName: 'Dmitriy', status: 'I am a boss too!', location: {city: 'Mykolaiv', country: 'Ukraine'},
-        photoUrl: 'https://lh3.googleusercontent.com/proxy/8tVV8AE9ZydHcHauEm3mh-VhTFai5gEdy_YOz9EHx2b4Rjdclu34u_xhC5x4B69L50-J5rah_4IBAk2KxIwaYwVEWE_K-6aYRjhvsHOfZu9GuFArx4k9_R2vbwi2Gs5GiYUtXJ40AjPI79-CaSZXw3Dz-0A' },
-      { id: 4, followed: true, fullName: 'Nata', status: 'I am a boss too!', location: {city: 'Odessa', country: 'Ukraine'},
-        photoUrl: 'https://lh3.googleusercontent.com/proxy/QAqVGbdZuQ-lwh88NVJIrkcnEv2aVQD_UseQoXzoAmktD1Yv5PTRDgw4ESDcp1hE-uSqo4U70thKqLmONdU5Bwkp7s79093LL8yI83q0M3xy' }
-    ])
+
+    axios.get("https://social-network.samuraijs.com/api/1.0/users")
+      .then(response => {
+        props.setUsers(response.data.items)
+      // props.setUsers([
+      //   { id: 1, followed: false,  fullName: 'Alexey', status: 'I am a boss!', location: {city: 'Kiev', country: 'Ukraine'},
+      //     photoUrl: 'https://lh3.googleusercontent.com/proxy/8tVV8AE9ZydHcHauEm3mh-VhTFai5gEdy_YOz9EHx2b4Rjdclu34u_xhC5x4B69L50-J5rah_4IBAk2KxIwaYwVEWE_K-6aYRjhvsHOfZu9GuFArx4k9_R2vbwi2Gs5GiYUtXJ40AjPI79-CaSZXw3Dz-0A' },
+      //   { id: 2, followed: true, fullName: 'Ilona', status: 'I am a boss too!', location: {city: 'Poltava', country: 'Ukraine'},
+      //     photoUrl: 'https://lh3.googleusercontent.com/proxy/QAqVGbdZuQ-lwh88NVJIrkcnEv2aVQD_UseQoXzoAmktD1Yv5PTRDgw4ESDcp1hE-uSqo4U70thKqLmONdU5Bwkp7s79093LL8yI83q0M3xy' },
+      //   { id: 3, followed: false, fullName: 'Dmitriy', status: 'I am a boss too!', location: {city: 'Mykolaiv', country: 'Ukraine'},
+      //     photoUrl: 'https://lh3.googleusercontent.com/proxy/8tVV8AE9ZydHcHauEm3mh-VhTFai5gEdy_YOz9EHx2b4Rjdclu34u_xhC5x4B69L50-J5rah_4IBAk2KxIwaYwVEWE_K-6aYRjhvsHOfZu9GuFArx4k9_R2vbwi2Gs5GiYUtXJ40AjPI79-CaSZXw3Dz-0A' },
+      //   { id: 4, followed: true, fullName: 'Nata', status: 'I am a boss too!', location: {city: 'Odessa', country: 'Ukraine'},
+      //     photoUrl: 'https://lh3.googleusercontent.com/proxy/QAqVGbdZuQ-lwh88NVJIrkcnEv2aVQD_UseQoXzoAmktD1Yv5PTRDgw4ESDcp1hE-uSqo4U70thKqLmONdU5Bwkp7s79093LL8yI83q0M3xy' }
+      // ])
+    })
   }
 
   return (
@@ -22,7 +29,7 @@ let Users = (props) => {
         props.users.map(u => <div key={u.id}>
             <div>
               <div>
-                <img src={u.photoUrl} className={styles.userPhoto} alt="photo"/>
+                <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto} alt="photo"/>
               </div>
               <div>
                 { u.followed
@@ -32,12 +39,12 @@ let Users = (props) => {
             </div>
             <div>
               <div>
-                <span>{u.fullName}</span>
+                <span>{u.name}</span>
                 <span>{u.status}</span>
               </div>
               <div>
-                <span>{u.location.country}</span>
-                <span>{u.location.city}</span>
+                <span>{"u.location.country"}</span>
+                <span>{"u.location.city"}</span>
               </div>
             </div>
           </div>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,16 +3,7 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 
 let initialState = {
-  users: [
-    { id: 1, followed: false,  fullName: 'Alexey', status: 'I am a boss!', location: {city: 'Kiev', country: 'Ukraine'},
-        photoUrl: 'https://lh3.googleusercontent.com/proxy/8tVV8AE9ZydHcHauEm3mh-VhTFai5gEdy_YOz9EHx2b4Rjdclu34u_xhC5x4B69L50-J5rah_4IBAk2KxIwaYwVEWE_K-6aYRjhvsHOfZu9GuFArx4k9_R2vbwi2Gs5GiYUtXJ40AjPI79-CaSZXw3Dz-0A' },
-    { id: 2, followed: true, fullName: 'Ilona', status: 'I am a boss too!', location: {city: 'Poltava', country: 'Ukraine'},
-        photoUrl: 'https://lh3.googleusercontent.com/proxy/QAqVGbdZuQ-lwh88NVJIrkcnEv2aVQD_UseQoXzoAmktD1Yv5PTRDgw4ESDcp1hE-uSqo4U70thKqLmONdU5Bwkp7s79093LL8yI83q0M3xy' },
-    { id: 3, followed: false, fullName: 'Dmitriy', status: 'I am a boss too!', location: {city: 'Mykolaiv', country: 'Ukraine'},
-        photoUrl: 'https://lh3.googleusercontent.com/proxy/8tVV8AE9ZydHcHauEm3mh-VhTFai5gEdy_YOz9EHx2b4Rjdclu34u_xhC5x4B69L50-J5rah_4IBAk2KxIwaYwVEWE_K-6aYRjhvsHOfZu9GuFArx4k9_R2vbwi2Gs5GiYUtXJ40AjPI79-CaSZXw3Dz-0A' },
-    { id: 4, followed: true, fullName: 'Nata', status: 'I am a boss too!', location: {city: 'Odessa', country: 'Ukraine'},
-        photoUrl: 'https://lh3.googleusercontent.com/proxy/QAqVGbdZuQ-lwh88NVJIrkcnEv2aVQD_UseQoXzoAmktD1Yv5PTRDgw4ESDcp1hE-uSqo4U70thKqLmONdU5Bwkp7s79093LL8yI83q0M3xy' }
-  ]
+  users: []
 }
 
 const usersReducer = (state = initialState, action) => {
