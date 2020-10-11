6ef2fa566efa526998c73b8e13a2685ba6554199
af4n <aleksey.afanaskin@gmail.com>
2020-10-11 16:20:03 +0300

    auth me was added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header/Header";
+import HeaderContainer from "./components/Header/HeaderContainer";
 import Navbar from "./components/Navbar/Navbar";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
@@ -14,7 +14,7 @@ import {Route} from "react-router-dom";
 const App = (props) => {
   return (
     <div className="app-wrapper">
-      <Header/>
+      <HeaderContainer />
       <Navbar/>
       <div className="app-wrapper-content">
         <Route path="/dialogs"
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import styles from "./Header.module.css";
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <header className={styles.header}>
-      <img
-        src="https://everlife.ai/images/muscari/header-image-muscari.png"
-        alt="logo"
-      />
-      <span>Social network</span> 
+      <div>
+        <img
+          src="https://everlife.ai/images/muscari/header-image-muscari.png"
+          alt="logo"
+        />
+        <span>Social network</span>
+      </div>
+      <div className={styles.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to={"/login"}>Login</NavLink>}
+      </div>
+
     </header>
   );
 };
