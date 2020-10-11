eada59ef8d8fc9993650e7a547a996748eff2340
af4n <aleksey.afanaskin@gmail.com>
2020-10-11 22:16:06 +0300

    follow/unfollow api was added

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Users.module.css"
 import userPhoto from "../../assets/images/user-photo.png"
 import {NavLink} from "react-router-dom";
+import * as axios from "axios";
 
 let Users = (props) => {
   let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -33,10 +34,30 @@ let Users = (props) => {
               <div>
                 {u.followed
                   ? <button onClick={() => {
-                    props.unfollow(u.id)
+                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                      withCredentials: true,
+                      headers: {
+                        "API-KEY": "74a5532c-9406-4acb-a75c-f9f31ea020a6"
+                      }
+                    })
+                      .then(response => {
+                        if (response.data.resultCode === 0) {
+                          props.unfollow(u.id)
+                        }
+                      })
                   }}>Unfollow</button>
                   : <button onClick={() => {
-                    props.follow(u.id)
+                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                      withCredentials: true,
+                      headers: {
+                        "API-KEY": "74a5532c-9406-4acb-a75c-f9f31ea020a6"
+                      }
+                    })
+                      .then(response => {
+                        if (response.data.resultCode === 0) {
+                          props.follow(u.id)
+                        }
+                      })
                   }}>Follow</button>}
               </div>
             </div>
