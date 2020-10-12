3fea9259bd7dff0603722003dcd72328148b29f5
af4n <aleksey.afanaskin@gmail.com>
2020-10-12 19:52:10 +0300

    redux-thunk was added

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Users.module.css"
 import userPhoto from "../../assets/images/user-photo.png"
 import {NavLink} from "react-router-dom";
-import * as axios from "axios";
+import {usersAPI} from "../../api/api";
 
 let Users = (props) => {
   let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
@@ -33,37 +33,10 @@ let Users = (props) => {
               </div>
               <div>
                 {u.followed
-                  ? <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
-                    props.toggleFollowingProgress(true, u.id)
-                    axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
-                      withCredentials: true,
-                      headers: {
-                        "API-KEY": "74a5532c-9406-4acb-a75c-f9f31ea020a6"
-                      }
-                    })
-                      .then(response => {
-                        if (response.data.resultCode === 0) {
-                          props.unfollow(u.id)
-                        }
-                        props.toggleFollowingProgress(false, u.id)
-                      })
-                  }}>Unfollow</button>
-                  : <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
-                    props.toggleFollowingProgress(true, u.id)
-                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
-                      withCredentials: true,
-                      headers: {
-                        "API-KEY": "74a5532c-9406-4acb-a75c-f9f31ea020a6"
-                      }
-                    })
-                      .then(response => {
-                        if (response.data.resultCode === 0) {
-                          props.follow(u.id)
-                        }
-                        props.toggleFollowingProgress(false, u.id)
-
-                      })
-                  }}>Follow</button>}
+                  ? <button disabled={props.followingInProgress.some(id => id === u.id)}
+                            onClick={() => {props.unfollow(u.id)}}>Unfollow</button>
+                  : <button disabled={props.followingInProgress.some(id => id === u.id)}
+                            onClick={() => {props.follow(u.id)}}>Follow</button>}
               </div>
             </div>
             <div>
--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,3 +1,5 @@
+import {authAPI} from "../api/api";
+
 const SET_AUTH_USER_DATA = 'SET_USER_DATA';
 
 
@@ -25,5 +27,17 @@ const authReducer = (state = initialState, action) => {
 }
 
 export const setAuthUserData = (id, login, email) => ({type: SET_AUTH_USER_DATA, data: {id, login, email}})
+
+export const getAuthUserData = () => {
+  return (dispatch) => {
+    authAPI.me()
+      .then(data => {
+        if (data.resultCode === 0) {
+          let {id, login, email} = data.data;
+          dispatch(setAuthUserData(id, login, email))
+        }
+      })
+  }
+}
 
 export default authReducer;
