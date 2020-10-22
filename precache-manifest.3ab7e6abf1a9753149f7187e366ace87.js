3d301450886a64f6dbac731cf5ebd6a8b213d11b
af4n <aleksey.afanaskin@gmail.com>
2020-10-22 15:34:26 +0300

    Updates

---
+++ precache-manifest.3ab7e6abf1a9753149f7187e366ace87.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac1df9aedb858f77d128911f604b96f4",
+    "url": "/react-social-network/index.html"
+  },
+  {
+    "revision": "4c8ff8c40203e6554578",
+    "url": "/react-social-network/static/css/main.979356d0.chunk.css"
+  },
+  {
+    "revision": "e01b556bee50db3b4e53",
+    "url": "/react-social-network/static/js/2.dc5399ce.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-social-network/static/js/2.dc5399ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c8ff8c40203e6554578",
+    "url": "/react-social-network/static/js/main.2debeffb.chunk.js"
+  },
+  {
+    "revision": "2148af4175143d2e0785",
+    "url": "/react-social-network/static/js/runtime-main.03ff3c32.js"
+  },
+  {
+    "revision": "0236b3123805731d5bff087f1e472f56",
+    "url": "/react-social-network/static/media/preloader.0236b312.gif"
+  },
+  {
+    "revision": "739297c3ec0f727c32c8135aca85df15",
+    "url": "/react-social-network/static/media/user-photo.739297c3.png"
+  }
+]);
